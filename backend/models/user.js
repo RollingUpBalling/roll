1723f31fe6c3388cb52ecbeff6b9448a832662cd
sverhrazum69 <n.osypa666@gmail.com>
2020-07-26 00:23:34 +0300

    created user model

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    steamUsername:{
+        type: String,
+        required: true,   
+    },
+    balance:{
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('User',User)
