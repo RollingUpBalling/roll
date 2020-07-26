0efd1a5e6f511121b5d2db88e2f23452780f114e
17etro <petrpetryk2016@gmail.com>
2020-07-26 11:46:49 +0300

    create socialNetworks links and sign up button

---
+++ frontend/src/components/Navigation/SocialLinks/SocialLinks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import classes from './SocialLinks.module.css';
+
+const socialLinks = ( props ) => (
+    <div className={classes.SocialLinks}>
+        <a href="https://web.telegram.org/">
+            <div 
+                className="fa fa-telegram" 
+                aria-hidden="true"></div>
+        </a>
+        <a href="/">
+            <div 
+                className="fa fa-instagram" 
+                aria-hidden="true"></div>
+        </a>
+        <a href="https://vk.com">
+            <div 
+                className="fa fa-vk" 
+                aria-hidden="true"></div>
+        </a>
+    </div>
+);
+
+export default socialLinks;
