1f7f356ec224a2711759380f5d8daebd255b783c
sverhrazum69 <n.osypa666@gmail.com>
2020-07-28 17:41:23 +0300

    updated models

--- backend/models/game.js
+++ backend/models/game.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Game = new Schema({
-    _id:{
-        type: String,
-        required:true   
-    },
     koef:{
         type: Number,               // we generate it from the start
         required: true
@@ -13,6 +9,11 @@ const Game = new Schema({
     amount:{
         type: Number,
         required: true,
+    },
+    state:{
+        type:String,
+        required: true,
+        default:'active'
     },
     bets:[{                                     // array of bets
         type: mongoose.Types.ObjectId,
