f5851ea91689663157d83e64c0064caf12979e9a
sverhrazum69 <n.osypa666@gmail.com>
2020-07-29 12:46:03 +0300

    added input  validation when createing bets

--- backend/routes/bet.js
+++ backend/routes/bet.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const { body } = require('express-validator')
+
 const betController = require('../controllers/bet');
 
 const isAuth = require('../middleware/auth')
 
-router.post('/makeBet/',isAuth,betController.makeBet)
+router.post('/makeBet/',[
+    body('steamUsername').isString(),
+    body('koef').isDecimal().custom(value => value > 0),
+    body('amount').isDecimal().custom(value => value > 0)
+],isAuth,betController.makeBet)
 
 module.exports = router
