4aac98352ee478eb95b3fe1ed4e152eb399caa7c
17etro <petrpetryk2016@gmail.com>
2020-07-29 13:50:33 +0300

    add bomb and static timer

---
+++ frontend/src/containers/Bomb/Bomb.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import bombIMG from '../../assets/images/bomb.png';
+import classes from './Bomb.module.css';
+
+class Bomb extends Component {
+    render() {
+        return (
+            <>
+            <img 
+                className={classes.Bomb} 
+                src={bombIMG} 
+                alt="Bomb"/>
+            <div className={classes.Board}>
+                <div class={classes.Koef}>
+                    <span>0</span>
+                    <span>0</span>
+                          .
+                    <span>0</span>
+                    <span>0</span>
+                    <span className={classes.AfterKoef}>sec</span>
+                </div>
+            </div>
+            </>
+        );
+    }
+};
+
+export default Bomb;
