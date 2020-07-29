561c0bf7254c847d7666d77fde7016e83a6a7b0a
sverhrazum69 <n.osypa666@gmail.com>
2020-07-29 12:58:49 +0300

    added validation when updating state

--- backend/controllers/game.js
+++ backend/controllers/game.js
@@ -1,19 +1,25 @@
 const Game = require('../models/game')
 const HttpError = require('../models/HttpError')
+const { validationResult } = require('express-validator')
 
-exports.updateState = async (req,res,next) => {
+exports.updateState = async (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            input : req.body.newState,
+            error: "invalid state, available options: 'finished', 'active', 'makingBets' "
+        })
+    }
     try {
-        const game = await Game.findOne().sort({'created_at':-1})
-        if (req.body.newState === 'active' || req.body.newState === 'makingBets' || req.body.newState === 'finished') {
-            game.state = req.body.newState
-            game.save()
-            res.sendStatus(204)
-        }
-        else {
-            res.status(400).json({
-                error:'invalid state'
+        const game = await Game.findOne().sort({ 'created_at': -1 })
+        if (!game) {
+            return res.status(500).json({
+                error: 'no game found'
             })
         }
+        game.state = req.body.newState
+        game.save()
+        res.sendStatus(204)
     } catch (error) {
         throw new HttpError(error)
     }
