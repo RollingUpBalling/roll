6ad60ec8e8edc7be39e0349b70208868e81cc4e1
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-30 21:50:56 +0300

    added backdrop and modal hooks

---
+++ frontend/src/components/UI/ErrorModal/BackDrop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './BackDrop.css';
+
+const Backdrop = props => {
+  return ReactDOM.createPortal(
+    <div className="backdrop" onClick={props.onClick}></div>,
+    document.getElementById('backdrop-hook')
+  );
+};
+
+export default Backdrop;
