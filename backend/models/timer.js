d22d57e2536cf063e2962eb41e68f8499fb40767
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-02 13:02:45 +0300

    deleted unnessessary code

---
+++ backend/models/timer.js
@@ -0,0 +1,16 @@
+const io = require('../socket');
+const socket = io.getIO();
+
+class Timer{
+    constructor(){
+        this.startTime = 30000
+    }
+
+    getCurrent(){
+        
+    }
+
+
+}
+
+module.exports = Timer;
