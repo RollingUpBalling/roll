1ee1b8b2ceb93ef8d02740f4d45d58bd9afdf25f
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-04 20:16:54 +0300

    added better use of useEffect

--- frontend/src/hoc/Layout/ProfileWrapperAuthorized/ProfileWrapperAuthorized.js
+++ frontend/src/hoc/Layout/ProfileWrapperAuthorized/ProfileWrapperAuthorized.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import classes from './Profile.module.css';
 import axios from 'axios'
@@ -8,51 +8,54 @@ import io from "socket.io-client";
 
 const ENDPOINT = "http://127.0.0.1:5000";
 
-const ProfileWrapperAuthorized = ( props ) => {
+const ProfileWrapperAuthorized = (props) => {
 
-    const [balance,updateBalance] = useState(1)
-    
-    useEffect(async () => {
+    const [balance, updateBalance] = useState(1)
+
+    useEffect(() => {
         try {
-            
-            const response = await axios.get(ENDPOINT+'/getUser/' + JSON.parse(localStorage.getItem('userData')).userId + '/')
-            updateBalance(response.data.balance)
-            const socket = io(ENDPOINT)
-            socket.on('newPhase',async data => {
-                if (data.state === 'finished') {
-                    const response = await axios.get(ENDPOINT+'/getUser/' + JSON.parse(localStorage.getItem('userData')).userId + '/')
-                    updateBalance(response.data.balance)
-                }
-            })
+            const fn = async () => {
+                const response = await axios.get(ENDPOINT + '/getUser/' + JSON.parse(localStorage.getItem('userData')).userId + '/')
+                updateBalance(response.data.balance)
+                const socket = io(ENDPOINT)
+                socket.on('newPhase', async data => {
+                    if (data.state === 'finished') {
+                        const response = await axios.get(ENDPOINT + '/getUser/' + JSON.parse(localStorage.getItem('userData')).userId + '/')
+                        updateBalance(response.data.balance)
+                    }
+                })
+            }
+            fn();
         } catch (error) { }
-        
-    },[])
+
+    }, [])
 
     return (
         <Aux>
+
             <div className={classes.DepositButton}>
                 <span>
                     Deposit
                     <i className="fa fa-money" aria-hidden="true"></i>
                 </span>
             </div>
-            <div style={{marginLeft: "15px"}}>
+            <div style={{ marginLeft: "15px" }}>
                 <p className={classes.Name}>
                     <a href="/">{JSON.parse(localStorage.getItem('userData')).username}</a>
                 </p>
                 <p className={classes.Balance}>
-                    {'$'+balance}
-                </p>    
+                    {'$' + balance}
+                </p>
             </div>
-            <SettingsButton 
-            showSettings={props.showSettings}
-            settingsHandler={props.settingsHandler}
-            closeSettings={props.closeSettings}
-            changeAnimation={props.changeAnimation}
-            animation={props.animation}/>
-            <div 
-            className={classes.LogOut}
-            onClick={props.clicked}>
+            <SettingsButton
+                showSettings={props.showSettings}
+                settingsHandler={props.settingsHandler}
+                closeSettings={props.closeSettings}
+                changeAnimation={props.changeAnimation}
+                animation={props.animation} />
+            <div
+                className={classes.LogOut}
+                onClick={props.clicked}>
                 <i className="fa fa-sign-out" aria-hidden="true" ></i>
             </div>
         </Aux>
