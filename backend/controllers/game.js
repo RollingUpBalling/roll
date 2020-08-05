0539c8849bdbb51e82d9a94056b269ee3fc5bb1d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-05 20:01:35 +0300

    added comments and deleted unnessasry code

--- backend/controllers/game.js
+++ backend/controllers/game.js
@@ -1,6 +1,5 @@
 const Game = require('../models/game')
 const HttpError = require('../models/HttpError')
-const bet = require('../models/bet')
 
 
 exports.createGame = async (req, res, next) => {
@@ -14,13 +13,13 @@ exports.createGame = async (req, res, next) => {
         })
         const io = require('../socket').getIO()
         io.emit('recieveId',{
-            'gameId' : game._id
+            'gameId' : game._id         //recieving game id
         });
         io.emit('newPhase',{
-            state:'makingBets'
+            state:'makingBets'              // adding ability to add bets
         })
         const inter1 = setInterval(function () {
-            if (game.timerStart <= 0) return;
+            if (game.timerStart <= 0) return;           // interval for the timer of the start game
             game.timerStart = game.timerStart-10;
             io.emit('timerStart', { 'numbers': game.timerStart });
         }, 10)
@@ -29,12 +28,12 @@ exports.createGame = async (req, res, next) => {
             
             game.state = 'active'
             game.save()
-            clearInterval(inter1);
+            clearInterval(inter1);      //clearing interval
             
             io.emit('newPhase',{
-                state:'active'
+                state:'active'              // staring our game
             })
-            let interval2 = setInterval(async function () {
+            let interval2 = setInterval(async function () {     // starting timer of the game with game koef
                 if (game.timerFinish >= game.koef*1000) return ;
                 game.timerFinish = game.timerFinish+10;
                 io.emit('timerFinish', { 'koef': game.timerFinish });
@@ -43,40 +42,40 @@ exports.createGame = async (req, res, next) => {
             setTimeout(async () => {
                 clearInterval(interval2);
                 
-                game.state = 'finished'
+                game.state = 'finished'     // setting game to finished
                 await game.save()
                 const currentGame = await Game.findOne().sort({ _id: -1 }).populate({
                     path:'bets',
                     populate:{
                         path:'user',
                         model:'User'
-                    }
+                    }           //finding bets and theirs users
                 })
                 currentGame.bets.forEach(async bet => {
                     if (!bet.won) {
                         if (bet.koef <= currentGame.koef) {
-                            bet.won = true
-                            bet.user.balance += bet.amount * bet.koef
+                            bet.won = true      //adding status
+                            bet.user.balance += bet.amount * bet.koef // adding balance
                             await bet.save()
                             await bet.user.save()
                         }
                         else {
                             bet.won = false
-                            await bet.save()
+                            await bet.save()        //adding status
                         }
                     }
                 });
-                await currentGame.save()
+                await currentGame.save()        //saves changes of the game
                
                 io.emit('newPhase',{
-                    state: 'crashed'
+                    state: 'crashed'        //adding state crashed
                 })  
                 io.emit('gameResults',{
-                    bets:currentGame.bets
+                    bets:currentGame.bets       
                 })
                 setTimeout(()=>{
                     io.emit('newPhase',{
-                        state:'finished'
+                        state:'finished'    
                     })
                 },2000)
             }, game.koef*10000 - 10000);
