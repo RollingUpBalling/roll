1933ce4d1c33979d19481b4cd2eae9408d1bb88e
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-07 15:25:43 +0300

    added route for deposit

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -6,7 +6,13 @@ const { body } = require('express-validator')
 const router = express.Router();
 
 
-router.post('/createGame/',isAuth,gameController.createGame)
+router.post('/createGame/', isAuth, gameController.createGame)
 // router.get('/handleResults/',gameController.handleResults)
+router.post(
+    '/deposit',
+    body('amount').isDecimal().custom(value => value > 0),
+    isAuth,
+    gameController.deposit
+)
 
 module.exports = router
