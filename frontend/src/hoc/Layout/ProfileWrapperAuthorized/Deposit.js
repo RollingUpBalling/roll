cffa4d7de6bad0710b3507cba03bd33b2260cb44
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-07 15:25:32 +0300

    added deposit link

---
+++ frontend/src/hoc/Layout/ProfileWrapperAuthorized/Deposit.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { Redirect, withRouter } from 'react-router-dom';
+import axios from 'axios';
+import classes from './Deposit.css';
+
+
+const ENDPOINT = "http://127.0.0.1:5000";
+
+const Deposit = (props) => {
+    console.log(props)
+    const [amount, setAmount] = useState(0);
+    const [pressed, setPressed] = useState(false);
+    const [data, setData] = useState();
+    const [sign, setSign] = useState();
+
+
+    const koefChangedHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
+
+    const createDepo = async () => {
+        try {
+            const res = await axios.post(ENDPOINT + '/deposit', { amount: amount }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userData')).token
+                }
+            });
+            console.log(res.data.data);
+            console.log(res.data.signature)
+            setData(res.data.data);
+            setSign(res.data.signature)
+            setPressed(true);
+        }
+        catch (err) {
+            console.log(err.response);
+        }
+
+    }
+
+    return (
+
+        <div>
+            <input
+                id="amount"
+
+                value={amount}
+                onChange={koefChangedHandler}
+                type='number'
+                min='0'
+                max='1000'
+                className={classes.KoefInput}
+            >
+            </input>
+            <button type="submit" onClick={createDepo}>
+                
+                asasd
+
+            </button>
+                <a href={`https://www.liqpay.ua/api/3/checkout?data=${data}&signature=${sign}`} >
+                    Pay
+                </a>
+        </div>
+
+    );
+}
+export default Deposit;
--- frontend/src/hoc/Layout/ProfileWrapperAuthorized/ProfileWrapperAuthorized.js
+++ frontend/src/hoc/Layout/ProfileWrapperAuthorized/ProfileWrapperAuthorized.js
@@ -1,56 +1,62 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import classes from './Profile.module.css';
 import axios from 'axios'
 import Aux from '../../../hoc/Auxillary/Auxillary';
 import SettingsButton from '../../../components/UI/SettingsButton/SettingsButton';
-import socket from '../../../socket'
+import Deposit from './Deposit';
+
 
 const ENDPOINT = "http://127.0.0.1:5000";
 
-const ProfileWrapperAuthorized = ( props ) => {
+const ProfileWrapperAuthorized = (props) => {
 
-    const [balance,updateBalance] = useState(1)
+    const [balance, updateBalance] = useState(1)
+
     
+
     useEffect(() => {
         try {
             const getBalance = async () => {
-                const response = await axios.get(ENDPOINT+'/getUser/' + JSON.parse(localStorage.getItem('userData')).userId + '/')
-                updateBalance(response.data.balance)    
+                const response = await axios.get(ENDPOINT + '/getUser/' + JSON.parse(localStorage.getItem('userData')).userId + '/')
+                updateBalance(response.data.balance)
             }
             getBalance()
         } catch (error) { }
 
-        
-        
-    },[])
+
+
+    }, [])
 
     return (
         <Aux>
             {}
-            <div className={classes.DepositButton}>
+            <div
+             
+             className={classes.DepositButton}>
+                 <Deposit />
                 <span>
                     Deposit
                     <i className="fa fa-money" aria-hidden="true"></i>
                 </span>
             </div>
-            <div style={{marginLeft: "15px"}}>
+            <div style={{ marginLeft: "15px" }}>
                 <p className={classes.Name}>
                     <a href="/">{JSON.parse(localStorage.getItem('userData')).username}</a>
                 </p>
                 <p className={classes.Balance}>
-                    {'$'+balance}
-                </p>    
+                    {'$' + balance}
+                </p>
             </div>
-            <SettingsButton 
-            showSettings={props.showSettings}
-            settingsHandler={props.settingsHandler}
-            closeSettings={props.closeSettings}
-            changeAnimation={props.changeAnimation}
-            animation={props.animation}/>
-            <div 
-            className={classes.LogOut}
-            onClick={props.clicked}>
+            <SettingsButton
+                showSettings={props.showSettings}
+                settingsHandler={props.settingsHandler}
+                closeSettings={props.closeSettings}
+                changeAnimation={props.changeAnimation}
+                animation={props.animation} />
+            <div
+                className={classes.LogOut}
+                onClick={props.clicked}>
                 <i className="fa fa-sign-out" aria-hidden="true" ></i>
             </div>
         </Aux>
