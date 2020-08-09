a593cdf7850d33347de5dc449c4ee1048f77814f
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-09 13:19:54 +0300

    added modal for display input

--- frontend/src/hoc/Layout/ProfileWrapperAuthorized/Deposit.js
+++ frontend/src/hoc/Layout/ProfileWrapperAuthorized/Deposit.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import classes from './Deposit.css';
+import Modal from '../../../components/UI/ErrorModal/Mod';
+//import classes from './Deposit.css';
+import classes from './Profile.module.css';
 
 
 const ENDPOINT = "http://127.0.0.1:5000";
 
 const Deposit = (props) => {
     const [amount, setAmount] = useState(0);
+    const [showInput, setShowInput] = useState(false);
 
+    const makeDeposit = () => {
+        setShowInput(true);
+    };
 
+    const goBack = () =>{
+        setShowInput(false);
+    }
 
     const koefChangedHandler = (event) => {
         setAmount(event.target.value);
@@ -23,7 +32,7 @@ const Deposit = (props) => {
                     'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userData')).token
                 }
             });
-            window.location = `https://www.liqpay.ua/api/3/checkout?data=${res.data.data}&signature=${res.data.signature}`; 
+            window.location = `https://www.liqpay.ua/api/3/checkout?data=${res.data.data}&signature=${res.data.signature}`;
         }
         catch (err) {
             console.log(err);
@@ -32,27 +41,35 @@ const Deposit = (props) => {
     }
 
     return (
-
-        <div>
-            <input
-                id="amount"
-
-                value={amount}
-                onChange={koefChangedHandler}
-                type='number'
-                min='0'
-                max='1000'
-                className={classes.KoefInput}
-                onSubmit={createDepo}
+        <>
+            <Modal
+                show={showInput}
+                onCancel = {goBack}
             >
-            </input>
-            <button type="submit" onClick={createDepo}>
-                Pay
-            </button>
-               {/*  <a href={``} >
+                <input
+                    id="amount"
+
+                    value={amount}
+                    onChange={koefChangedHandler}
+                    type='number'
+                    min='0'
+                    max='1000'
+                    className={classes.KoefInput}
+                    onSubmit={createDepo}
+                >
+                </input>
+                <button
+                    type="submit"
+                    onClick={createDepo}
+                >
                     Pay
-                </a> */}
-        </div>
+                </button>
+            </Modal>
+            <span onClick={makeDeposit}>
+                Deposit
+            <i className="fa fa-money" aria-hidden="true"></i>
+            </span>
+        </>
 
     );
 }
