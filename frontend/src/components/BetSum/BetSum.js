42fe86c570547492b4b92b87a47ef90b12baa3de
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-09 23:08:14 +0300

    added pattern for amount bet

--- frontend/src/components/BetSum/BetSum.js
+++ frontend/src/components/BetSum/BetSum.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
+import SignInButton from '../../components/UI/SignInButton/SignInButton';
+import { AuthContext } from '../../context/auth-context';
 import classes from './BetSum.module.css';
 
-const betSum = ( props ) => {
+const BetSum = (props) => {
+    const auth = useContext(AuthContext);
+    const [amount, setAmount] = useState(0);
+
+    const koefChangedHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
     return (
         <div className={classes.BetSumBody}>
+            {
+                auth.isLoggedIn ? (
+                    <>
+                        <header>
+                            Amount of bet
+                        </header>
+                        <input
+                            value={amount}
+                            type='number'
+                            min='0'
+                            max='1000'
+                            onChange={koefChangedHandler}
+                        />
+                    </>
+                ) :
+                    (
+                        <>
+                            <h1>You need to login with steam</h1>
+
+                            <SignInButton />
+                        </>
+                    )
+            }
         </div>
     );
 };
 
-export default betSum;
+export default BetSum;
